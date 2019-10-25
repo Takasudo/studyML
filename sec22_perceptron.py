7a2d312db308f5d6e5c6b19661fb1082b2229473
Sudoh Takahiro <taka@sudoh-astron.local>
2019-10-25 11:39:13 +0900

    second commit

---
+++ sec22_perceptron.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Perceptron(object):
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, Y):
+        ##
+        return self
+
+    def net_input(self, X):
+        #
+        return #
+
+    def predict(self, X):
+        #
+        return #
